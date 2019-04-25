d7b82c13f4b4ef7265eb0fff00e56e1a3001c5b8
Koneko11 <cerberusdonaldson@yahoo.com>
2019-04-25 00:37:35 +0000

    js changes

--- js/main.js
+++ js/main.js
@@ -117,3 +117,120 @@ function equalClick()
 	
 }//end of function
 alert("hello3")
+
+//function clear Button
+
+function clearButton(){
+	num1 = "";
+	num2 = "";
+	flag = 0;
+	display.innerHTML = "";
+	equalTo = 0;
+}//end of function
+
+//function backSpace
+
+function backspace(){
+	var temp1 ="";
+	var temp2 ="";
+	if(equalTo === 1){
+		clearButton();
+	}//end of if
+
+	if(flag === 0){
+		temp1 = num1.substring(0, num1.length-1);
+		//alert(temp1)
+		display.innerHTML = temp1;
+		num1 = temp1;
+	}//end of if
+
+	if(flag === 1){
+		display.innerHTML = num1;
+		flag = 0;
+	}//end of if
+
+	if(num2 !== ""){
+		temp2 = num2.substring(0, num2.length-1);
+		display.innerHTML = num1 + operator + num2;
+		num2 =temp2;
+		flag = 1;
+		setOppString();
+
+		/*if (operator === 1) {
+			display.innerHTML = num1 + "+" + num2; //we are doing this
+		} else if (operator === 2) {
+			display.innerHTML = num1 + "-" + num2;
+
+		} else if (operator === 3) {
+			display.innerHTML = num1 + "*" + num2;
+
+		} else if (operator === 4) {
+			display.innerHTML = num1 + "/" + num2;
+		}// end of if */
+
+	}//end of if
+
+}//end of function
+
+//function setDecimal
+
+function setDecimal(){
+	if(flag === 0){
+		if(num1 === ""){
+			num1 = "0.";
+			display.innerHTML = num1;
+
+		}//end of 1st if
+
+		if(num1.indexOf('.')=== -1){
+			num1+= ".";
+			display.innerHTML = num1;
+
+		}//end of 2nd if
+
+	}//end of if
+	if (flag === 1){
+		if(num2 === ""){
+			num2 = "0.";
+			display.innerHTML+= num2;
+		}//end of 1st if
+
+		if(num2.index('.') === -1){
+			num2+= ".";
+			display.innerHTML = num1 + operator + num2;
+
+			if(operator === 1) {
+				if (operator === 1) {
+			display.innerHTML = num1 + "+" + num2; //we are doing this
+		} else if (operator === 2) {
+			display.innerHTML = num1 + "-" + num2;
+
+		} else if (operator === 3) {
+			display.innerHTML = num1 + "*" + num2;
+
+		} else if (operator === 4) {
+			display.innerHTML = num1 + "/" + num2;
+		}//end of if
+
+			}//end of 2nd if
+		}//end of if flag
+
+	}
+
+}//end of function
+
+// function setOppString
+function setOppString(){
+/*if(operator === 1) {
+				if (operator === 1) {
+			display.innerHTML = num1 + "+" + num2; //we are doing this
+		} else if (operator === 2) {
+			display.innerHTML = num1 + "-" + num2;
+
+		} else if (operator === 3) {
+			display.innerHTML = num1 + "*" + num2;
+
+		} else if (operator === 4) {
+			display.innerHTML = num1 + "/" + num2;
+		}//end of if */
+}
